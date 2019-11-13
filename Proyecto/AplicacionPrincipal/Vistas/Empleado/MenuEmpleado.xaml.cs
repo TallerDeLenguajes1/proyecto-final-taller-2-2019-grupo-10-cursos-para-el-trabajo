c93206e2104df597efae610fb35c249d1f173974
FacuCR <facucastro302@gmail.com>
2019-11-12 23:49:24 -0300

    Eliminar desde el formulario Tutor y Instructor en la bd

--- Proyecto/AplicacionPrincipal/Vistas/Empleado/MenuEmpleado.xaml.cs
+++ Proyecto/AplicacionPrincipal/Vistas/Empleado/MenuEmpleado.xaml.cs
@@ -26,12 +26,43 @@ namespace AplicacionPrincipal.Vistas.Empleado
         List<Tutor> tutores;
         MySqlConnection conn;
         string mensaje;
+        List<int> idesInstructores;
+        List<int> idesTutores;
+
         public MenuEmpleado()
         {
             InitializeComponent();
 
             instructores = new List<Instructor>();
             tutores = new List<Tutor>();
+            idesInstructores = new List<int>();
+            idesTutores = new List<int>();
+
+            btnEliminarEmpleado.IsEnabled = false;
+            btnModificarEmpleado.IsEnabled = false;
+
+            ActualizarListBox();
+        }
+
+        public void ActualizarListBox()
+        {
+            lbxEmpleados.Items.Clear();
+
+            ConexionEmpleado.GetInstructores(instructores, idesInstructores);
+
+            ConexionEmpleado.GetTutores(tutores, idesTutores);
+
+            foreach (var instructor in instructores)
+            {
+                lbxEmpleados.Items.Add(instructor);
+            }
+
+            foreach (var tutor in tutores)
+            {
+                lbxEmpleados.Items.Add(tutor);
+            }
+
+            lbxEmpleados.Items.Refresh();
         }
 
         private void btnAltaEmpleado_Click(object sender, RoutedEventArgs e)
@@ -64,9 +95,73 @@ namespace AplicacionPrincipal.Vistas.Empleado
                     mensaje = ConexionEmpleado.AgregarTutor(conn, frmAltaEmpleado.GetTutor());
                 }
 
+                ActualizarListBox();
+
                 MessageBox.Show(mensaje);
 
                 conn = Conexion.Desconectar();
+            }
+        }
+
+        private void btnModificarEmpleado_Click(object sender, RoutedEventArgs e)
+        {
+            ABMModificarEmpleado frmModificarEmpleado = new ABMModificarEmpleado();
+
+            frmModificarEmpleado.ShowDialog();
+        }
+
+        private void btnEliminarEmpleado_Click(object sender, RoutedEventArgs e)
+        {
+            int id = lbxEmpleados.SelectedIndex;
+
+            string mensaje;
+
+            try
+            {
+                conn = Conexion.Conectar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            if (instructores.Count > lbxEmpleados.SelectedIndex)
+            {
+                mensaje = ConexionEmpleado.EliminarInstructor(conn, idesInstructores[id]);
+
+                //MessageBox.Show(instructores[lbxEmpleados.SelectedIndex].Nombre + " - " + idesInstructores[lbxEmpleados.SelectedIndex]);
+            }
+            else
+            {
+                id = lbxEmpleados.SelectedIndex - instructores.Count;
+
+                mensaje = ConexionEmpleado.EliminarTutor(conn, idesTutores[id]);
+                //MessageBox.Show(tutores[id].Apellido + " - " + idesTutores[id]);
+            }
+
+            MessageBox.Show(mensaje);
+
+            conn = Conexion.Desconectar();
+        }
+
+        private void btnVolver_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        /// <summary>
+        /// Metodo para habilitar el boton de Eliminar y Modificar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lbxEmpleados_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {  
+            if (lbxEmpleados.SelectedIndex != -1)
+            {
+                btnModificarEmpleado.IsEnabled = true;
+
+                btnEliminarEmpleado.IsEnabled = true;
             }
         }
     }
