1275c2b9fde944dab6d3c250fcd1269828f8485c
FacuCR <facucastro302@gmail.com>
2019-11-13 23:21:13 -0300

    Modificar Instructor y Tutor

---
+++ Proyecto/AplicacionPrincipal/Vistas/Beneficiariosaf/MenuBeneficiario.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AplicacionPrincipal.Vistas.Beneficiario
+{
+    /// <summary>
+    /// Lógica de interacción para MenuBeneficiario.xaml
+    /// </summary>
+    public partial class MenuBeneficiario : Window
+    {
+        public MenuBeneficiario()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAltaBeneficiario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnModificarBeneficiario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEliminarBeneficiario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnVolver_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LbxBeneficiario_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
